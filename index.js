d8ab1ab3fd2458fdee1b2da1628c44ac44744e7f
thonn@fimplus.vn <thonn@fimplus.vn>
2020-07-28 10:24:59 +0700

    update

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import path from 'path';
-import bodyParser from 'body-parser';
-import appRouter from './routes';
-import serveStatic from 'serve-static'
-import dataShopHelper from './dataShopHelper'
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import bodyParser from "body-parser";
+import appRouter from "./routes";
+import serveStatic from "serve-static";
+import dataShopHelper from "./dataShopHelper";
 
 const app = express();
-const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
+const DATABASE_URL =
+  process.env.DATABASE_URL || "mongodb://localhost:27017/test";
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
-db.once('open', () => {
-    console.log(`Database connection in ${DATABASE_URL}`);
+db.once("open", () => {
+  console.log(`Database connection in ${DATABASE_URL}`);
 });
 
-app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, "./public/src")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(appRouter);
 
-app.listen(3001, () => console.log('server is running in port 3001'));
+app.listen(3001, () => console.log("server is running in port 3001"));
---
+++ public/src/assets/js/app.js
@@ -0,0 +1,84 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+function MainController($scope, $route, $routeParams, $location) {
+  console.log("vao");
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+  $scope.redirectPage = (e, path) => {
+    e.preventDefault();
+    $location.path(path);
+  };
+}
+
+function HomeController($scope, $http) {
+  $scope.title = "List Product";
+  // $scope.userName = '';
+  // $scope.userId = 0;
+  $scope.countModel = "5";
+  $scope.currenPage = 1;
+  $scope.totalPage = [];
+  $scope.loading = false;
+
+  $scope.getListProduct = (number = null, type = null) => {
+    $scope.loading = true;
+    console.log("current", $scope.currenPage + "number", number + "type", type);
+    if (number) $scope.currenPage = number;
+    if (type) {
+      switch (type) {
+        case "prev":
+          $scope.currenPage = $scope.currenPage - 1;
+          break;
+        case "next":
+          $scope.currenPage = $scope.currenPage + 1;
+          break;
+      }
+    }
+    console.log($scope.countModel, $scope.currenPage);
+    $http
+      .get(`/product?limit=${$scope.countModel}&page=${$scope.currenPage}`)
+      .then((result) => {
+        console.log(result.data.product);
+        $scope.listProduct = result.data.product;
+        let totalPage = [];
+        for (let i = 1; i <= result.data.totalPage; i++) {
+          totalPage.push(i);
+        }
+        $scope.totalPage = totalPage;
+        $scope.loading = false;
+      });
+  };
+
+  $scope.getListProduct();
+}
+
+function ProductController($scope, $http, $routeParams) {
+  let productId = $routeParams.productId;
+  let varientId = $routeParams.varientId;
+  $scope.getProductInfo = (productId, variantId) => {
+    $http
+      .get(`/product/info?productId=${productId}&variantId=${variantId}`)
+      .then((result) => {
+        console.log("product", result);
+        $scope.product = result.data;
+      });
+  };
+  $scope.getProductInfo(productId, varientId);
+}
+
+app
+  .controller("MainController", MainController)
+  .controller("HomeController", HomeController)
+  .controller("ProductController", ProductController);
+
+app.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when("/product/:productId/:varientId", {
+      templateUrl: "product.html",
+      controller: "ProductController",
+    })
+    .when("/", {
+      templateUrl: "home.html",
+      controller: "HomeController",
+    });
+});
